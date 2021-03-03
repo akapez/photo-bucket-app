e44294a75c4f279ff5848d260c0abde984ddcf12
avishka964 <avishkakapuruge@gmail.com>
2021-03-03 11:43:57 +0530

    database connect

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'
+import connectDB from './config/db.js'
+import dotenv from 'dotenv'
 
 dotenv.config()
 
-const app = express()
+connectDB()
 
+const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
