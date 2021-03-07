f7cfc70fb83cd9a279b12c915e86c6d1c9170742
avishka964 <avishkakapuruge@gmail.com>
2021-03-07 23:57:33 +0530

    user login screen set

---
+++ frontend/src/screens/Login.js
@@ -0,0 +1,200 @@
+import React from "react"
+import {
+  Grid,
+  makeStyles,
+  Card,
+  CardContent,
+  MenuItem,
+  InputLabel,
+  Select,
+  CardActions,
+  Button,
+  CardHeader,
+  FormControl,
+} from "@material-ui/core"
+
+import { Formik, Form, Field } from "formik"
+import * as Yup from "yup"
+import { TextField } from "formik-material-ui"
+
+const useStyle = makeStyles((theme) => ({
+  padding: {
+    padding: theme.spacing(3),
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}))
+
+//Data
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  occupation: "",
+  city: "",
+  country: "",
+  email: "",
+  password: "",
+}
+
+const options = [
+  { label: "Computer Programmer", value: "Computer_programmer" },
+  { label: "Web Developer", value: "web_developer" },
+  { label: "User Experience Designer", value: "user_experience_designer" },
+  { label: "Systems Analyst", value: "systems_analyst" },
+  { label: "Quality Assurance Tester", value: "quality_assurance_tester" },
+]
+
+//password validation
+const lowercaseRegEx = /(?=.*[a-z])/
+const uppercaseRegEx = /(?=.*[A-Z])/
+const numericRegEx = /(?=.*[0-9])/
+const lengthRegEx = /(?=.{6,})/
+
+//validation schema
+let validationSchema = Yup.object().shape({
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .matches(
+      lowercaseRegEx,
+      "Must contain one lowercase alphabetical character!"
+    )
+    .matches(
+      uppercaseRegEx,
+      "Must contain one uppercase alphabetical character!"
+    )
+    .matches(numericRegEx, "Must contain one numeric character!")
+    .matches(lengthRegEx, "Must contain 6 characters!")
+    .required("Required!"),
+})
+
+const UserForm = () => {
+  const classes = useStyle()
+
+  const onSubmit = (values) => {
+    console.log(values)
+  }
+
+  return (
+    <Grid container justify="center" spacing={1}>
+      <Grid item md={6}>
+        <Card className={classes.padding}>
+          <CardHeader title="REGISTER FORM"></CardHeader>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}>
+            {({ dirty, isValid, values, handleChange, handleBlur }) => {
+              return (
+                <Form>
+                  <CardContent>
+                    <Grid item container spacing={1} justify="center">
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="First Name"
+                          variant="outlined"
+                          fullWidth
+                          name="firstName"
+                          value={values.firstName}
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="Last Name"
+                          variant="outlined"
+                          fullWidth
+                          name="lastName"
+                          value={values.lastName}
+                          component={TextField}
+                        />
+                      </Grid>
+
+                      <Grid item xs={12} sm={6} md={12}>
+                        <FormControl fullWidth variant="outlined">
+                          <InputLabel id="demo-simple-select-outlined-label">
+                            Occupation
+                          </InputLabel>
+                          <Select
+                            labelId="demo-simple-select-outlined-label"
+                            id="demo-simple-select-outlined"
+                            label="Occupation"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.occupation}
+                            name="occupation">
+                            <MenuItem>None</MenuItem>
+                            {options.map((item) => (
+                              <MenuItem key={item.value} value={item.value}>
+                                {item.label}
+                              </MenuItem>
+                            ))}
+                          </Select>
+                        </FormControl>
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="City"
+                          variant="outlined"
+                          fullWidth
+                          name="city"
+                          value={values.city}
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="Country"
+                          variant="outlined"
+                          fullWidth
+                          name="country"
+                          value={values.country}
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="Email"
+                          variant="outlined"
+                          fullWidth
+                          name="email"
+                          value={values.email}
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="Password"
+                          variant="outlined"
+                          fullWidth
+                          name="password"
+                          value={values.password}
+                          type="password"
+                          component={TextField}
+                        />
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      disabled={!dirty || !isValid}
+                      variant="contained"
+                      color="primary"
+                      type="Submit"
+                      className={classes.button}>
+                      REGISTER
+                    </Button>
+                  </CardActions>
+                </Form>
+              )
+            }}
+          </Formik>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default UserForm
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,21 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {postListReducer} from './reducers/postReducers'
+import {userLoginReducer} from './reducers/userReducers'
 
 const reducer = combineReducers({
     postList: postListReducer,
+    userLogin: userLoginReducer
     
 })
 
-const initialState = {}
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null
+
+const initialState = {
+    userLogin: {userInfo: userInfoFromStorage}
+}
 
 const middleware = [thunk]
 
