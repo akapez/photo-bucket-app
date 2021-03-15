44af532e3f3dfe7da4a12d417a8a4cb4f1b0920b
avishka964 <avishkakapuruge@gmail.com>
2021-03-15 16:43:55 +0530

    issus fixed

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express()
 
 app.use(express.json())
 
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
@@ -24,20 +24,20 @@ app.use('/api/posts', postRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/upload', uploadRoutes)
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")))
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  )
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running")
-  })
-}
-
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
